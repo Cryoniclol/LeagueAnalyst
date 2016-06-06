af71313200adb9cabdca2650152ad0c74be202f9
Lukas_Fend <lukas.fend@student.htldornbirn.at>
2016-06-06 10:29:53 +0100

    General changes...

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # LeagueAnalyst
 League Analyst is an online tool for displaying **League of Legends** statistics/profiles etc.  
 The data is gathered from the [official Riot Games API](https://developer.riotgames.com).  
-
+  
 ## Configuration
-League Analyst is configured in the **index.php** file,
-you can find settings like the API-Key in that file.
-** Please use your own API Key!**
-You can get one under [this](https://developer.riotgames.com/sign-in) site.
+League Analyst is configured in the **index.php** file,  
+you can find settings like the API-Key in that file.  
+** Please use your own API Key!**  
+You can get one under [this](https://developer.riotgames.com/sign-in) site.  
--- pages/matchhistory.php
+++ pages/matchhistory.php
@@ -28,7 +28,7 @@ $GCH = new GameConstantsHandler();
 
 <?php
     $playerboards=[];
-    for($i=0;$i<9;$i++)
+    for($i=0;$i<=9;$i++)
     {
         $p1 = $game["participants"][$i];
         $p1Ident = $game["participantIdentities"][$i];
@@ -71,6 +71,51 @@ for($i=0;$i<5;$i++)
     ".$playerboards[$i]["data"]["stats"]["assists"]." - ".$kda." KDA
 </td>";
 
+    // SPACER
+    if($i==0) {
+        echo "<td rowspan='11'> <h2>VS</h2> </td>";
+    }
+    $i+=5;
+    $kda = round(($playerboards[$i]["data"]["stats"]["assists"] + $playerboards[$i]["data"]["stats"]["kills"])/$playerboards[$i]["data"]["stats"]["deaths"],2);
+
+    // PLAYER 2
+    if($kda < 1) {
+        $kda = "<span style='color:red'>" . $kda . "</span>";
+    } else if($kda < 4) {
+        $kda = "<span style='color:green'>" . $kda . "</span>";
+    } else {
+        $kda = "<span style='color:orange;font-weight: bold;'>" . $kda . "</span>";
+    }
+    echo "<td rowspan='2'>
+    ".$playerboards[$i]["data"]["stats"]["kills"]."/
+    ".$playerboards[$i]["data"]["stats"]["deaths"]."/
+    ".$playerboards[$i]["data"]["stats"]["assists"]." - ".$kda." KDA
+</td>";
+
+
+    echo "<td rowspan='2'>" . $playerboards[$i]["data"]["stats"]["minionsKilled"] . " CS</td>";
+
+
+    echo "<td>
+<img src='".$imgItemPath . $playerboards[$i]["data"]["stats"]["item0"] . ".png' width='25px' height='25px' onError='this.src=\"http://fs5.directupload.net/images/160602/gntez3mx.jpg\";'>
+<img src='".$imgItemPath . $playerboards[$i]["data"]["stats"]["item1"] . ".png' width='25px' height='25px' onError='this.src=\"http://fs5.directupload.net/images/160602/gntez3mx.jpg\";'>
+<img src='".$imgItemPath . $playerboards[$i]["data"]["stats"]["item2"] . ".png' width='25px' height='25px' onError='this.src=\"http://fs5.directupload.net/images/160602/gntez3mx.jpg\";'>
+    </td>";
+
+
+
+
+
+    echo "<td rowspan='2'><strong>".$playerboards[$i]["name"]."</strong></td>";
+    echo "<td><img src='".$imgPath . "/summspells/" . $playerboards[$i]["data"]["spell1Id"] . ".png' width='25px' height='25px'></td>";
+    echo "<td rowspan='2'><img src='".$imgPath . "/squares/" . $playerboards[$i]["data"]["championId"] . ".png' height='50px' width='50px'></td>";
+
+    // End Player 2
+
+    $i-=5;
+
+
+
 
     echo "</tr><tr><td>
 <img src='".$imgPath . "/summspells/" . $playerboards[$i]["data"]["spell2Id"] . ".png' width='25px' height='25px'>
@@ -81,7 +126,28 @@ for($i=0;$i<5;$i++)
 <img src='".$imgItemPath . $playerboards[$i]["data"]["stats"]["item3"] . ".png' width='25px' height='25px' onError='this.src=\"http://fs5.directupload.net/images/160602/gntez3mx.jpg\";'>
 <img src='".$imgItemPath . $playerboards[$i]["data"]["stats"]["item4"] . ".png' width='25px' height='25px' onError='this.src=\"http://fs5.directupload.net/images/160602/gntez3mx.jpg\";'>
 <img src='".$imgItemPath . $playerboards[$i]["data"]["stats"]["item5"] . ".png' width='25px' height='25px' onError='this.src=\"http://fs5.directupload.net/images/160602/gntez3mx.jpg\";'>
-    </td></tr>";
+    </td>";
+
+
+    echo "<td>
+<img src='".$imgItemPath . $playerboards[$i+5]["data"]["stats"]["item3"] . ".png' width='25px' height='25px' onError='this.src=\"http://fs5.directupload.net/images/160602/gntez3mx.jpg\";'>
+<img src='".$imgItemPath . $playerboards[$i+5]["data"]["stats"]["item4"] . ".png' width='25px' height='25px' onError='this.src=\"http://fs5.directupload.net/images/160602/gntez3mx.jpg\";'>
+<img src='".$imgItemPath . $playerboards[$i+5]["data"]["stats"]["item5"] . ".png' width='25px' height='25px' onError='this.src=\"http://fs5.directupload.net/images/160602/gntez3mx.jpg\";'>
+    </td>";
+
+    echo "<td>
+<img src='".$imgPath . "/summspells/" . $playerboards[$i+5]["data"]["spell2Id"] . ".png' width='25px' height='25px'>
+</td></tr>";
+
+
+
+
+
+}
+if($game["teams"][0]["winner"]) {
+    echo "<tr><td style='color:green;text-align: center;' colspan='6'><h2>Win</h2></td><td style='color:red;text-align: center;' colspan='6'><h2>Lose</h2></td></tr>";
+} else {
+    echo "<tr><td style='color:red;text-align: center;' colspan='6'><h2>Lose</h2></td><td style='color:green;text-align: center;' colspan='6'><h2>Win</h2></td></tr>";
 }
 ?>
         </table>
